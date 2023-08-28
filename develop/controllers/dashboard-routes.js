9c455ab7fb45eabea5d568b1f347b1a1f77ef65c
lugo.kse23@gmail.com <lugo.kse23@gmail.com>
2023-08-28 00:04:43 -0700

    controllers underway!

---
+++ develop/controllers/dashboard-routes.js
@@ -0,0 +1,50 @@
+const router = require('express').Router();
+const { Post } = require('../models/');
+const withAuth = require('../utils/auth');
+
+//get method to retrieve all post data with authentication
+router.get('/', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      where: {
+        userId: req.session.userId,
+      },
+    });
+
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    res.render('all-posts-admin', {
+      layout: 'dashboard',
+      posts,
+    });
+  } catch (err) {
+    res.redirect('login');
+  }
+});
+//get method to retrieve all new posts in the /new endpoint with authentication
+router.get('/new', withAuth, (req, res) => {
+  res.render('new-post', {
+    layout: 'dashboard',
+  });
+});
+//get method to retrieve all post data that has been edited
+router.get('/edit/:id', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id);
+
+    if (postData) {
+      const post = postData.get({ plain: true });
+
+      res.render('edit-post', {
+        layout: 'dashboard',
+        post,
+      });
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
+    res.redirect('login');
+  }
+});
+
+module.exports = router;
